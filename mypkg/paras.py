582490a40b051f8846a0cefe6dca19eb113f5c2a
Huaqing Jin <Kevinjin0423@gmail.com>
2022-12-28 16:19:42 -0800

    add code for checking the stability

--- mypkg/paras.py
+++ mypkg/paras.py
@@ -1,23 +1,23 @@
 # this file contains parameters for implementing FC-SBI-SGM
 from easydict import EasyDict as edict
+import numpy as np
 
 
-paras = edict()
+paras_cons = edict()
+paras_cons.delta = [2, 3.5]
+paras_cons.theta = [4, 7]
+paras_cons.alpha = [8, 12]
+paras_cons.beta = [13, 20]
 
-paras.res_dir = Path("./results")
+# Parameter bounds for optimization
+v_lower = 3.5-1.8
+v_upper = 3.5+1.8
+bnds = ((0.005,0.030), (0.005,0.2), (0.005,0.030), (v_lower,v_upper), (0.1,1.0), (0.5,10.0), (0.5,10.0))
+#taue,taui,tauG,speed,alpha,gii,gei
 
-paras.fc_type = "cohy" #mag, coh
-#paras.freqrange = [8, 13] # frequency rage
-paras.freqrange =  fvec[np.bitwise_and(fvec>8, fvec<13)]
-paras.fs = 600
-paras.num_nodes = 86 # Number of cortical (68) + subcortical nodes
-paras.uni_SC = False
+paras_cons.fs = 600
+paras_cons.num_nodes = 86 # Number of cortical (68) + subcortical nodes
+paras_cons.par_low =  np.array([ix[0] for ix in bnds])
+paras_cons.par_high = np.array([ix[1] for ix in bnds])
+paras_cons.prior_bds = np.array([paras.par_low, paras.par_high]).T
 
-paras.SBI_paras = edict()
-paras.SBI_paras.num_prior_sps = int(1e5)
-#paras.SBI_paras.density_model = "nsf"
-paras.SBI_paras.density_model = "mdn"
-paras.SBI_paras.noise_sd = 0.15
-paras.SBI_paras.combine_loss = False
-paras.SBI_paras.prefix = "" # ModSC: modified SC, MR: multiple round, None
-#paras.SBI_paras.prefix = "ModSC"
