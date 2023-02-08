4b0e91e89e9259e1f5dee36a12d1a724d5d34c99
Huaqing Jin <Kevinjin0423@gmail.com>
2023-02-08 01:56:21 -0800

    modify the way to calculate FC a bit

--- mypkg/FC_utils.py
+++ mypkg/FC_utils.py
@@ -16,13 +16,12 @@ def build_fc_freq_m(brain, params , freqrange):
     estFC, the mean normalized estimated FC at the given frequency computed 
             over the range given in freqrange.
     """
-    model_out = 0
+    estFC = 0
     for cur_freq in freqrange:
-        cur_model_out, _, _, _ = nt.network_transfer_local_alpha(brain , params, cur_freq)
-        model_out = cur_model_out/len(freqrange) + model_out
-
-    # No noise matrix P(\omega) explicitly used here
-    estFC = np.matmul( model_out , np.matrix.getH(model_out) )
+        model_out, _, _, _ = nt.network_transfer_local_alpha(brain , params, cur_freq)
+        # No noise matrix P(\omega) explicitly used here
+        cur_estFC = np.matmul(model_out , np.matrix.getH(model_out) )
+        est_FC = cur_estFC/len(freqrange) + estFC
 
     # Now normalize estFC
     diagFC = np.diag(np.abs(estFC))
@@ -33,3 +32,36 @@ def build_fc_freq_m(brain, params , freqrange):
     estFC = estFC - np.diag(np.diag( estFC ))
 
     return estFC
+
+# def build_fc_freq_m(brain, params , freqrange):
+# 
+#     """
+#     Input:
+#     
+#     brain: brain model
+#     params: brain parameters
+#     freqrange: a struct containing the frequency range (bandwidth) of interest, in Hz, with 
+#     ranges alpha, beta, delta, theta, gamma.
+# 
+#     Output:
+# 
+#     estFC, the mean normalized estimated FC at the given frequency computed 
+#             over the range given in freqrange.
+#     """
+#     model_out = 0
+#     for cur_freq in freqrange:
+#         cur_model_out, _, _, _ = nt.network_transfer_local_alpha(brain , params, cur_freq)
+#         model_out = cur_model_out/len(freqrange) + model_out
+# 
+#     # No noise matrix P(\omega) explicitly used here
+#     estFC = np.matmul( model_out , np.matrix.getH(model_out) )
+# 
+#     # Now normalize estFC
+#     diagFC = np.diag(np.abs(estFC))
+#     diagFC = 1./np.sqrt(diagFC)
+#     D = np.diag( diagFC )
+#     estFC = np.matmul( D , estFC )
+#     estFC = np.matmul( estFC , np.matrix.getH(D) ) # f_ij/\sqrt(f_ii)\sqrt(f_jj)
+#     estFC = estFC - np.diag(np.diag( estFC ))
+# 
+#     return estFC
